a384427a63bb7f30d1640f751fb74731aa2ebb02
Bogdan Teslarasu <bogdantsu@yahoo.com>
2015-10-02 01:09:12 +0300

    Finished homework.

--- SuperTable.java
+++ SuperTable.java
@@ -1,62 +1,97 @@
-import java.io.IOException;
-
+import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
-
-import org.apache.hadoop.hbase.TableName;
-
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
-
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
-public class SuperTable{
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-   public static void main(String[] args) throws IOException {
+public class SuperTable {
 
-      // Instantiate Configuration class
+	public static final String POWERS_TABLE_NAME = "powers";
+	public static final String PERSONAL_COLUMN = "personal";
+	public static final String PROFESSIONAL_COLUMN = "professional";
 
-      // Instaniate HBaseAdmin class
-      
-      // Instantiate table descriptor class
+	private Configuration configuration;
+	private String tableName;
 
-      // Add column families to table descriptor
+	public SuperTable(Configuration configuration){
+		this.configuration = configuration;
+	}
 
-      // Execute the table through admin
+	public static void main(String[] args) throws IOException {
 
-      // Instantiating HTable class
-     
-      // Repeat these steps as many times as necessary
+		SuperTable superTable = new SuperTable(new HBaseConfiguration());
+		superTable.create(POWERS_TABLE_NAME, PERSONAL_COLUMN, PROFESSIONAL_COLUMN);
+		List<List<byte[]>> rowValues = Lists.newArrayList();
+		//	superman
+		rowValues.add(toRow("personal", "hero", "superman"));
+		rowValues.add(toRow("personal", "power", "strength"));
+		rowValues.add(toRow("professional", "name", "clark"));
+		rowValues.add(toRow("professional", "xp", "100"));
+		superTable.addRow("row1", rowValues);
+		rowValues.clear();
+		//	batman
+		rowValues.add(toRow("personal", "hero", "batman"));
+		rowValues.add(toRow("personal", "power", "money"));
+		rowValues.add(toRow("professional", "name", "wayne"));
+		rowValues.add(toRow("professional", "xp", "50"));
+		superTable.addRow("row2", rowValues);
+		rowValues.clear();
+		//	wolverine
+		rowValues.add(toRow("personal", "hero", "wolverine"));
+		rowValues.add(toRow("personal", "power", "stealing"));
+		rowValues.add(toRow("professional", "name", "logan"));
+		rowValues.add(toRow("professional", "xp", "75"));
+		superTable.addRow("row3", rowValues);
 
-	      // Instantiating Put class
-              // Hint: Accepts a row name
+		superTable.scan("personal", "hero");
+	}
 
-      	      // Add values using add() method
-              // Hints: Accepts column family name, qualifier/row name ,value
+	private void create(String tableName, String... columnFamilies) throws IOException {
+		this.tableName = tableName;
+		HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
+		for(String columnFamily: columnFamilies){
+			tableDescriptor.addFamily(new HColumnDescriptor(columnFamily));
+		}
+		new HBaseAdmin(configuration).createTable(tableDescriptor);
+		System.out.println(String.format("%s table has been created.", tableName));
+	}
 
-      // Save the table
-	
-      // Close table
+	private void addRow(String id, List<List<byte[]>> values) throws IOException {
+		HTable table = new HTable(configuration, tableName);
+		Put row = new Put(Bytes.toBytes(id));
+		for(List<byte[]> rowValues: values){
+			row.add(rowValues.get(0), rowValues.get(1), rowValues.get(2));
+		}
+		table.put(row);
+		table.close();
+		System.out.println(String.format("Added row with id '%s' into '%s' table.", id, tableName));
+	}
 
-      // Instantiate the Scan class
-     
-      // Scan the required columns
+	private void scan(String... familyColumn) throws IOException {
+		HTable table = new HTable(configuration, tableName);
+		Scan scanInfo = new Scan();
+		for(int i = 0; i < familyColumn.length; i+=2){
+			scanInfo.addColumn(Bytes.toBytes(familyColumn[i]), Bytes.toBytes(familyColumn[i+1]));
+		}
+		ResultScanner scanner = table.getScanner(scanInfo);
+		for(Result result = scanner.next(); result != null; result = scanner.next()){
+			System.out.println("Found row: " + result);
+		}
+		scanner.close();
+	}
 
-      // Get the scan result
-
-      // Read values from scan result
-      // Print scan result
- 
-      // Close the scanner
-   
-      // Htable closer
-   }
+	private static List<byte[]> toRow(String familyName, String qualifier, String value) throws IOException {
+		List<byte[]> row = new ArrayList<>();
+		row.add(Bytes.toBytes(familyName));
+		row.add(Bytes.toBytes(qualifier));
+		row.add(Bytes.toBytes(value));
+		return row;
+	}
 }
 
